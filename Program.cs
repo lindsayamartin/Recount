0c999d50c6cdcfc252b939ef0628b5c15ca8ccf4
Lindsay <Lindsay@DESKTOP-MRT9GBF>
2021-10-03 10:16:20 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Recount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, int> reCount = new Dictionary<string, int>();
+            for (int i = 0; i < 100000; i++)
+            {
+                string voteName = Console.ReadLine().ToUpper();
+                if (voteName == "***")
+                {
+                    break;
+                }
+                else
+                {
+                    if (reCount.ContainsKey(voteName))
+                    {
+                        reCount[voteName] += 1;
+                    }
+                    else
+                    {
+                        reCount.Add(voteName, 1);
+                    }
+                }
+            }
+            int maxVote = reCount.Values.Max();
+            int x = 0;
+            foreach (var vote in reCount)
+            {
+                if (vote.Value == maxVote)
+                {
+                    x++;
+                }
+            }
+            if (x < 2)
+            {
+                foreach (var vote in reCount)
+                {
+                    if (vote.Value == maxVote)
+                    {
+                        string maxKey = vote.Key;
+                        Console.WriteLine(maxKey);
+                        return;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Runoff!");
+            }
+        }
+    }
+}
